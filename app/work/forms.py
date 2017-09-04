3b1a6515c7732ba82b992f1efb2d0f99b64b5215
DT <dt.zhangjia@gmail.com>
2017-09-04 17:32:49 +0800

    approve process

--- app/work/forms.py
+++ app/work/forms.py
@@ -4,7 +4,7 @@ learn flask work
 # pylint: disable=invalid-name
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField, PasswordField, BooleanField, ValidationError # pylint: disable=C0301
+from wtforms import StringField, SelectField, TextAreaField, SubmitField, PasswordField, BooleanField, ValidationError # pylint: disable=C0301
 from wtforms.validators import Required, Length, Regexp
 from ..models import Payments, Approvers
 
@@ -25,3 +25,14 @@ class PayapplyForm(FlaskForm):
             dpt_all.append((str(dpt.idapprover),dpt.dpt))
         self.dpt_apply_input.choices = dpt_all
 
+
+class Paydetail(FlaskForm):
+    """Payment detail for approver"""
+    applier_view =  StringField('申请人', render_kw={'readonly': True})
+    applytime_view = StringField('申请时间', render_kw={'readonly': True})
+    dpt_view = StringField('使用部门', render_kw={'readonly': True})
+    budgettype_view = StringField('费用类型', render_kw={'readonly': True})
+    amount_view = StringField('申请金额', render_kw={'readonly': True})
+    content_view = TextAreaField('申请内容', render_kw={'readonly': True})
+    opinion = SelectField('审批意见', validators=[Required()], choices = [("", "---"), ("1","同意"),("0","拒绝")]) # pylint: disable=C0301
+    submit_view_btn = SubmitField('提交审批')
--- app/work/views.py
+++ app/work/views.py
@@ -2,23 +2,86 @@
 learn flask work views
 """
 # pylint: disable=invalid-name, too-few-public-methods
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 from flask_moment import Moment
 from datetime import datetime
 from .. import mydb
-from .forms import PayapplyForm
-
-
+from .forms import PayapplyForm, Paydetail
 from . import work
 from ..models import Payments, Approvers, User
+
+
 
 @work.route('/payment')
 @login_required
 def payment():
     """payment page"""
-    all_payments = Payments.query.all()
-    return render_template('work/payment.html', paylist=all_payments)
+    all_payments = Payments.query.order_by(Payments.applytime.desc()).all()
+
+    #准备全员姓名传入视图页面
+    all_users = User.query.all()
+    all_users_dict = {}
+    for user in all_users:
+        all_users_dict[user.uid] = user.name
+
+    return render_template('work/payment.html', paylist=all_payments, all_users_dict=all_users_dict)
+
+
+@work.route('/approvelist')
+@login_required
+def approvelist():
+    """待审批列表"""
+    all_approves = Payments.query.order_by(Payments.applytime.desc()).filter_by(approveruid=current_user.uid)
+    #准备全员姓名传入视图页面
+    all_users = User.query.all()
+    all_users_dict = {}
+    for user in all_users:
+        all_users_dict[user.uid] = user.name
+
+    return render_template('work/approvelist.html', 
+                           approvelist=all_approves, 
+                           all_users_dict=all_users_dict)
+
+
+@work.route('/paydetail/<pid>', methods=['GET', 'POST'])
+@login_required
+def paydetail(pid):
+    """申请详情及审批操作视图"""
+    #表单实例
+    paydetail_app = Paydetail()
+
+    #判断当前用户是否为审批人
+    paydetail_query = Payments.query.filter_by(idpayment=pid).first()
+    if current_user.uid != paydetail_query.approveruid:
+        flash('你无权审批此申请.')
+        return redirect(url_for('main.index'))
+
+    #准备数据
+    applier = User.query.filter_by(uid=paydetail_query.applieruid).first()
+    department = Approvers.query.filter_by(idapprover=paydetail_query.dpt).first()
+
+    #填充数据
+    paydetail_app.applier_view.data = applier.name
+    paydetail_app.applytime_view.data = paydetail_query.applytime
+    paydetail_app.dpt_view.data = department.dpt
+    paydetail_app.budgettype_view.data = paydetail_query.budgettype
+    paydetail_app.amount_view.data = paydetail_query.amount
+    paydetail_app.content_view.data = paydetail_query.content.decode()
+
+    #提交审批 出问题了 不是更新 而是新增了
+    if paydetail_app.validate_on_submit():
+        if paydetail_app.opinion.data =="1":
+            paydetail_query.opinion = True
+        elif paydetail_app.opinion.data =="0":
+            paydetail_query.opinion = False
+        mydb.session.add(paydetail_query)# pylint: disable=no-member
+        mydb.session.commit()# pylint: disable=no-member 需要立刻提交数据库以获得id
+        return redirect(url_for('work.approvelist'))
+
+
+    return render_template('work/paydetail.html', paydetail_disp=paydetail_app)
+
 
 @work.route('/payapply', methods=['GET', 'POST'])
 @login_required
